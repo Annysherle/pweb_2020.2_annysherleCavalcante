dda20ebe1992527bc6125e344e8572f7bb85b1b8
Annysherle <annysherlecavalcante@gmail.com>
2021-05-12 23:59:43 -0300

    .....

--- AgroPopShop/src/main/java/br/com/annysherle/AgroPopShop/api/controllers/PedidoController.java
+++ AgroPopShop/src/main/java/br/com/annysherle/AgroPopShop/api/controllers/PedidoController.java
@@ -2,69 +2,46 @@ package br.com.annysherle.AgroPopShop.api.controllers;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 import br.com.annysherle.AgroPopShop.api.models.Pedido;
 import br.com.annysherle.AgroPopShop.api.repository.PedidoRepository;
-/*
+
 @RestController
-@RequestMapping("/")
+@RequestMapping(value = "/ pedido")
 public class PedidoController {
 
-	@Autowired
-	PedidoRepository pedidoRepo;
+	PedidoRepository pedidoRepository;
+
+	public PedidoController(PedidoRepository pedidoRepository) {
+		super();
+		this.pedidoRepository = pedidoRepository;
+	}
+
+	@PostMapping
+	public Pedido save(@RequestBody Pedido pedido) {
+		return pedidoRepository.save(pedido);
+	}
 
 	@GetMapping
-	public String index() {
-		return "index.html";
+	public List<Pedido> listaTodos() {
+		return pedidoRepository.findAll();
 	}
 
-	@GetMapping("/listarPedido")
-	public ModelAndView listarPedido() {
-		List<Pedido> lista = pedidoRepo.findAll();
-		ModelAndView modelAndView = new ModelAndView("listarPedidos");
-		modelAndView.addObject("pedidos", lista);
-		return modelAndView;
+	@PutMapping
+	public void update(@RequestBody Pedido pedido) {
+		pedidoRepository.save(pedido);
 	}
 
-	@GetMapping("/adicionarPedido")
-	public ModelAndView formAdicionarPedido() {
-		ModelAndView modelAndView = new ModelAndView("adicionarPedido");
-		modelAndView.addObject(new Pedido());
-		return modelAndView;
-	}
-
-	@PostMapping("/adicionarPedido")
-	public String AdicionarProduto(Pedido p) {
-		this.pedidoRepo.save(p);
-		return "redirect:/listarPedidos";
-	}
-
-	@GetMapping("/editar/{id}")
-	public ModelAndView formEditarPedido(@PathVariable("id") long id) {
-		Pedido pedido = pedidoRepo.findById(id).orElseThrow(() -> new IllegalArgumentException("ID inválido" + id));
-		ModelAndView modelAndView = new ModelAndView("editarPedido");
-		modelAndView.addObject(pedido);
-		return modelAndView;
-	}
-
-	@PostMapping("/editar/{id}")
-	public ModelAndView editarPedido(@PathVariable("id") long id, Pedido pedido) {
-		this.pedidoRepo.save(pedido);
-		return new ModelAndView("redirect:/listarPedidos");
-	}
-
-	@GetMapping("/remover/{id}")
-	public ModelAndView removerProduto(@PathVariable("id") long id) {
-		Pedido aRemover = pedidoRepo.findById(id).orElseThrow(() -> new IllegalArgumentException("ID inválido" + id));
-		pedidoRepo.delete(aRemover);
-		return new ModelAndView("redirect:/listarPedidos");
+	@DeleteMapping("/{id}")
+	public void apagar(@PathVariable Long id) {
+		pedidoRepository.deleteById(id);
 	}
 }
-*/
